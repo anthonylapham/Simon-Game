d5cd99f0403fa40c9cc65f1a2482b5cec1c74f2d
Anthony Lapham <lapham.2@wright.edu>
2017-10-08 12:06:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//set up document
+//set counter. needs to go to 20 rounds for win
+//random start and random sequence
+//use slice for start and stop of each round
+// var sequence = [];
+// for (var i = 0; i < 20; i++) {
+//   sequence.push(Math.floor(Math.random() * 4))
+// }
+// console.log(sequence);
+var sequence2 = _.times(20, _.random(0,3));
+console.log(sequence2);
+
+
+//finish pseudo code
+//change buttons to divs
+//have sound and flash for all 20 numbers in sequence
+//flash lasts 1 full second or 1000 miliseconds
+//pause between should be half a second or 500 miliseconds
