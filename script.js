ed4320918ece4e5f9c143e2de0970892710e3580
Anthony Lapham <lapham.2@wright.edu>
2017-10-14 12:53:33 -0400

    style

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-//set up document
-//set counter. needs to go to 20 rounds for win
-//random start and random sequence
-//use slice for start and stop of each round
 // var sequence = [];
 // for (var i = 0; i < 20; i++) {
 //   sequence.push(Math.floor(Math.random() * 4))
 // }
 // console.log(sequence);
-var sequence2 = _.times(20, _.random(0,3));
+var sequence2 = _.times(20, _.random(0, 3));
 console.log(sequence2);
 
 
