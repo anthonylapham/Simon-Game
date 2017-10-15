7f6d32d56aca1240941ae3d47eed5bf0905d86c1
Anthony Lapham <lapham.2@wright.edu>
2017-10-14 22:00:12 -0400

    sound attempt

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+//generate an array of 20 numbers
+//put game parameters in global variable/function
+//in game variable set count to zero
+//in game variable set current game as []
+//in game variable set player as []
+//make new game function that correlates to default game function
+//creat click event for start button that starts game and auto increments count to 1
+//
+
+});
+
 // var sequence = [];
 // for (var i = 0; i < 20; i++) {
 //   sequence.push(Math.floor(Math.random() * 4))
@@ -5,6 +17,15 @@
 // console.log(sequence);
 var sequence2 = _.times(20, _.random(0, 3));
 console.log(sequence2);
+
+$('red').on('click', function(){
+  var audio = [];
+  audio["red"] = new Audio();
+  audio["red"].src = "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
+  audio["red"].addEventListener('load', function () {
+      audio["red"].play();
+});
+    });
 
 
 //finish pseudo code
